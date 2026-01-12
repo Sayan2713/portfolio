4fcf93aafb0fbd3c4b7a6bde92f04251f23087b8
Sayan2713 <sayanmonadl619@gmail.com>
2026-01-12 21:59:18 +0530

    Hero Section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Hero } from "./components/hero/hero";
+import { HeroComponent } from './components/hero/hero'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
-  templateUrl: './app.html',
+  standalone: true,
+  imports: [ HeroComponent], 
+  templateUrl: './app.html', 
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('my-portfolio');
+export class AppComponent {
+  title = 'my-portfolio';
 }
